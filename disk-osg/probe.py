7a9c6b629d9d3cf53238db0318eb8743646f72e3
Nathan Baltzell <baltzell@gmx.com>
2021-03-30 13:55:52 -0400

    allow limiting to held jobs

--- disk-osg/probe.py
+++ disk-osg/probe.py
@@ -4,28 +4,31 @@ import re
 import os
 import sys
 import glob
+import json
 import argparse
 import subprocess
 
+json_format =  {'indent':2, 'separators':(',',': '), 'sort_keys':True}
 log_regex = '/([a-z]+)/job_([0-9]+)/log/job\.([0-9]+)\.([0-9]+)\.'
 
-cli = argparse.ArgumentParser()
+condor_data = None
 
-sub_clis = cli.add_subparsers(dest='command')
-sub_clis.required = True
-
-cli_list = sub_clis.add_parser('list')
-cli_list.add_argument('-condor', default=[], action='append', type=str, help='limit by condor cluster id')
-cli_list.add_argument('-gemc', default=[], action='append', type=str, help='limit by gemc job id')
-cli_list.add_argument('-user', default=[], action='append', type=str, help='limit by user name')
-
-cli_dump = sub_clis.add_parser('dump')
-cli_dump.add_argument('-condor', default=[], action='append', type=str, help='limit by condor cluster id')
-cli_dump.add_argument('-tail', default=0, metavar='#', type=int, help='dump last # lines of logs (default=0, all=negative)')
-
-args = cli.parse_args(sys.argv[1:])
+def condor_query(cluster_id):
+  '''Get the JSON for a particular job from condor_q
+  cluster_id must be fully qualified, e.g. #####.##'''
+  global condor_data
+  if condor_data is None:
+    cmd = ['condor_q','gemc','-nobatch','-json']
+    condor_data = json.loads(subprocess.check_output(cmd).decode('UTF-8'))
+  for x in condor_data:
+    if 'ClusterId' in x and 'ProcId' in x:
+      if '%d.%d' % (x['ClusterId'],x['ProcId']) == cluster_id:
+        return x
+  return None
 
 def readlines_reverse(filename,max_lines):
+  '''Get the trailing lines from a file, stopping
+  after max_lines unless max_lines is negative'''
   n_lines = 0
   with open(filename) as qfile:
     qfile.seek(0, os.SEEK_END)
@@ -35,7 +38,10 @@ def readlines_reverse(filename,max_lines):
       if n_lines > max_lines and max_lines>0:
         break
       qfile.seek(position)
-      next_char = qfile.read(1)
+      try:
+        next_char = qfile.read(1)
+      except:
+        next_char = ''
       if next_char == "\n":
          n_lines += 1
          yield line[::-1]
@@ -45,52 +51,64 @@ def readlines_reverse(filename,max_lines):
       position -= 1
   yield line[::-1]
 
-# crawl the log directory, linking condor/gemc job ids, user names, and log files:
-data = {}
-for dirpath,dirnames,filenames in os.walk('/osgpool/hallb/clas12/gemc'):
-
-  for filename in filenames:
-
-    fullfilepath = dirpath+'/'+filename
-    m = re.search(log_regex,fullfilepath)
-    if m is None:
-      continue
-
-    user = m.group(1)
-    gemc = m.group(2)
-    condor = m.group(3)+'.'+m.group(4)
-
-    if condor not in data:
-      data[condor] = {'gemc':gemc, 'user':user, 'logs':[]}
-
-    data[condor]['logs'].append(fullfilepath)
-
-# print request based on command-line arguments:
-for key,val in sorted(data.items()):
-
-  if len(args.condor) > 0:
-    if key not in args.condor:
-      if key.split('.').pop(0) not in args.condor:
+def crawl():
+  '''Crawl the log directory, linking condor/gemc
+  job ids, user names, and log files'''
+  ret = {}
+  for dirpath,dirnames,filenames in os.walk('/osgpool/hallb/clas12/gemc'):
+    for filename in filenames:
+      fullfilepath = dirpath+'/'+filename
+      m = re.search(log_regex,fullfilepath)
+      if m is None:
         continue
+      user = m.group(1)
+      gemc = m.group(2)
+      condor = m.group(3)+'.'+m.group(4)
+      if condor not in ret:
+        ret[condor] = {'gemc':gemc, 'user':user, 'logs':[]}
+      ret[condor]['logs'].append(fullfilepath)
+  return ret
 
-  # just list jobs: 
-  if args.command == 'list':
+if __name__ == '__main__':
+
+  cli = argparse.ArgumentParser()
+  cli.add_argument('-condor', default=[], metavar='# or #.#', action='append', type=str, help='limit by condor cluster id')
+  cli.add_argument('-gemc', default=[], metavar='#', action='append', type=str, help='limit by gemc job id')
+  cli.add_argument('-user', default=[], action='append', type=str, help='limit by user name')
+  cli.add_argument('-held', default=False, action='store_true', help='limit to jobs currently in held state')
+  cli.add_argument('-tail', default=None, metavar='#', type=int, help='dump last # lines of logs (all=negative, 0=just-names)')
+  cli.add_argument('-json', default=False, action='store_true', help='dump JSON')
+
+  args = cli.parse_args(sys.argv[1:])
+
+  for condor,val in sorted(crawl().items()):
+
+    if len(args.condor) > 0:
+      if condor not in args.condor:
+        if condor.split('.').pop(0) not in args.condor:
+          continue
+
     if len(args.gemc) > 0 and val['gemc'] not in args.gemc:
       continue
+
     if len(args.user) > 0 and val['user'] not in args.user:
       continue
-    print('%16s %10s %12s' % (key,val['gemc'],val['user']))
 
-  # not only list, but also dump logs and directory contents:
-  elif args.command == 'dump':
-    print('\n\n::: Next Job ::::::::::::::::::::::::::::::::::::::::::::::::\n')
-    print('%16s %10s %12s' % (key,val['gemc'],val['user']))
-    print()
-    for x in val['logs']:
-      print('::: Next Log :::\n')
-      print(x)
-      if args.tail != 0:
-        print('\n'.join(readlines_reverse(x, args.tail)))
-      print()
+    if args.held is not None:
+      if condor_query(condor) is None:
+        continue
+      if condor_query(condor).get('JobStatus') != 5:
+        continue
 
+    print('%16s %10s %12s' % (condor,val['gemc'],val['user']))
+
+    if args.json:
+      print(json.dumps(condor_query(condor),**json_format))
+
+    if args.tail is not None:
+      for x in val['logs']:
+        print(x)
+        if args.tail != 0:
+          print('\n'.join(readlines_reverse(x, args.tail)))
+          print()
 
