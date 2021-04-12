8e68b63f0a3c912b9db13f2c37948bd6fa8303d1
Nathan Baltzell <baltzell@gmx.com>
2021-04-12 17:46:14 -0400

    prep vacate cronjob

---
+++ disk-osg/vacate-stalls.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+cache=$HOME/vacate-stalls.txt
+
+touch $cache
+
+$dirname/condor-probe.py -vacate 18 >> $cache
+
+if ! [ -z $1 ]; then
+    if [ $(cat $cache | wc -l) -ne 0 ]; then
+        echo "Vacated jobs in the past 24 hours:"
+        cat $cache
+    fi
+    rm -f $cache
+fi
+
