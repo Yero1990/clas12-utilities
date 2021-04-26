0d724d136ef0580d65e047d37ab9a52ead96534c
Nathan Baltzell <baltzell@gmx.com>
2021-04-26 10:36:30 -0400

    fix check-cvmfs cron message

--- disk-osg/check-cvmfs.sh
+++ disk-osg/check-cvmfs.sh
@@ -13,7 +13,7 @@ if ! [ -z $1 ]; then
     # 1. remove username, keeping only site, host, and job ids
     # 2. sort and remove uniques, i.e. same host and job id
     # 3. count per host and sort by counts
-    sed 's/@/ /g' $cache | awk '{print$1,$(NF-1),$NF}' | sort | uniq | awk '{print$1,$2}' | sort | uniq -c | sort -n -r
+    sed 's/@/ /g' $cache | awk '{print$1,$(NF-1),$NF}' | sort | uniq | awk '{print$1,$2}' | sort | uniq -c | sort -n -r > $tmp
     if [ $(cat $tmp | wc -l) -ne 0 ]; then
         echo "Nodes with CVMFS issues in the past 24 hours:"
         cat $tmp
