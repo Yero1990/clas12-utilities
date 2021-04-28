f8a5709727bcff738994fa4147167a2ee63b215e
Nathan Baltzell <baltzell@gmx.com>
2021-04-28 10:08:44 -0400

    vacate at 18.5 for SU dvcsgen

--- disk-osg/vacate-stalls.sh
+++ disk-osg/vacate-stalls.sh
@@ -4,13 +4,15 @@ dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 cache=$HOME/vacate-stalls.txt
 
+limit=18.5
+
 touch $cache
 
-$dirname/condor-probe.py -vacate 16 >> $cache
+$dirname/condor-probe.py -vacate $limit >> $cache
 
 if ! [ -z $1 ]; then
     if [ $(cat $cache | wc -l) -ne 0 ]; then
-        echo "Vacated jobs in the past 24 hours:"
+        echo "Vacated jobs longer than $limit hours in the past 24 hours:"
         cat $cache
     fi
     rm -f $cache
