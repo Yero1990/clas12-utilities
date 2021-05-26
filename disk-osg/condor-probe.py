30343b9b29632eb8c74dfdd1960f8bafc8e59db1
Nathan Baltzell <baltzell@gmx.com>
2021-05-26 13:06:39 -0400

    accommadate changing exit codes ...

--- disk-osg/condor-probe.py
+++ disk-osg/condor-probe.py
@@ -28,7 +28,7 @@ log_regex = '/([a-z]+)/job_([0-9]+)/log/job\.([0-9]+)\.([0-9]+)\.'
 job_states = {0:'U', 1:'I', 2:'R', 3:'X', 4:'C', 5:'H', 6:'E'}
 job_counts = {'done':0, 'run':0, 'idle':0, 'held':0, 'other':0, 'total':0}
 exit_codes = { 202:'cvmfs', 203:'generator', 211:'ls', 204:'gemc', 0:'success/unknown',
-               205:'evio2hipo', 207:'recon-util', 208:'hipo-utils', 210:'xrootd'}
+               205:'evio2hipo', 207:'recon-util', 208:'hipo-utils', 212:'xrootd'}
 cvmfs_error_strings = [ 'Loaded environment state is inconsistent',
   'Command not found','Unable to access the Singularity image','CVMFS ERROR']
 #  'No such file or directory', 'Transport endpoint is not connected',
@@ -393,7 +393,7 @@ def condor_exit_code_summary(args):
   tot = sum(x.values())
   ret = '\nExit Code Summary:\n'
   ret += '------------------------------------------------\n'
-  ret += '\n'.join(['%3d  %6.2f%%  %s'%(k,v/tot*100,exit_codes[k]) for k,v in x.items()])
+  ret += '\n'.join(['%4s  %6.2f%%  %s'%(k,v/tot*100,exit_codes.get(k)) for k,v in x.items()])
   return ret + '\n'
 
 def condor_efficiency_summary():
