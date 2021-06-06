e6a376b4ae4a2606f06f7e18959a808604844d17
Nathan Baltzell <baltzell@gmx.com>
2021-06-06 09:28:30 -0400

    no timeline archive, just you snapshotted filesytem

--- disk-osg/condor-probe.py
+++ disk-osg/condor-probe.py
@@ -15,6 +15,7 @@ import json
 import time
 import math
 import gzip
+import stat
 import shutil
 import socket
 import getpass
@@ -346,12 +347,12 @@ def condor_cluster_summary(args):
     ret[cluster_id]['done'] -= ret[cluster_id]['idle']
     ret[cluster_id]['done'] -= ret[cluster_id]['run']
     try:
+      if job['NumJobStarts'] > 0:
+        ret[cluster_id]['att'].append(job['NumJobStarts'])
       x = float(job['eff'])
       ret[cluster_id]['eff'].append(x)
       x = float(job['ceff'])
       ret[cluster_id]['ceff'].append(x)
-      if job['NumJobStarts'] > 0:
-        ret[cluster_id]['att'].append(job['NumJobStarts'])
     except:
       pass
   for v in ret.values():
@@ -788,19 +789,25 @@ def get_generator(job):
           break
   return generators.get('ClusterId')
 
-def timeline(args):
+def make_timeline_entry(args):
+  data = {}
+  summary = job_counts.copy()
+  #condor = {}
+  for cid,job in condor_cluster_summary(args).items():
+    #try:
+    #  condor[cid] = {'attempts':int(job['att'])}
+    #except:
+    #  pass
+    for x in summary.keys():
+      summary[x] += job[x]
+  summary.pop('done')
+  summary.pop('total')
   attempts = []
   for condor_id,job in condor_yield(args):
     try:
       attempts.append(int(job['NumJobStarts']))
     except:
       pass
-  summary = job_counts.copy()
-  for job in condor_cluster_summary(args).values():
-    for x in summary.keys():
-      summary[x] += job[x]
-  summary.pop('done')
-  summary.pop('total')
   summary['attempts'] = 0
   if len(attempts) > 0:
     summary['attempts'] = round(sum(attempts) / len(attempts),2)
@@ -808,26 +815,48 @@ def timeline(args):
   for site,val in condor_site_summary(args).items():
     if site is not None:
       sites[site] = val['run']
-  data = {}
   data['global'] = summary
   data['sites'] = sites
+  #data['condor'] = condor
   data['update_ts'] = int(datetime.datetime.now().timestamp())
-  path = '/osgpool/hallb/clas12/gemc/timeline.json'
+  return data
+
+def timeline(args):
+  data = make_timeline_entry(args)
+  basename = 'timeline.json'
+  srcdir = os.getenv('HOME')
   webdir = '/u/group/clas/www/clasweb-2015/html/clas12offline/osg'
-  #print(json.dumps(data, **json_format))
-  #return
-  if not os.path.exists(path) or os.access(path, os.W_OK):
-    with open(path,'r') as f:
+  srcpath = '%s/%s'%(srcdir,basename)
+  webpath = '%s/%s'%(webdir,basename)
+  cache = []
+  perms = stat.S_IRWXU & (stat.S_IRUSR|stat.S_IWUSR)
+  perms |= stat.S_IRWXG & (stat.S_IRGRP)
+  perms |= stat.S_IRWXO & (stat.S_IROTH)
+  try:
+    os.chmod(srcpath, perms)
+  except:
+    pass
+  if os.path.exists(srcpath) and os.access(srcpath, os.R_OK):
+    with open(srcpath,'r') as f:
       cache = json.load(f)
-    with open(path,'w') as f:
-      cache.append(data)
+  cache.append(data)
+  if not os.path.exists(srcpath) or os.access(srcpath, os.W_OK):
+    with open(srcpath,'w') as f:
       f.write(json.dumps(cache))
-    shutil.copy(path,webdir)
-    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
-    archive = webdir+'/archive/timeline-%s.json'%now
-    shutil.copy(path,archive)
-    cmd = ['find',webdir+'/archive/','-name','*.json','-ctime','+14','-delete']
-    subprocess.check_output(cmd)
+  else:
+    print('Archive DNE or unwritable:  '+srcpath)
+    print(json.dumps(cache,**json_format))
+  if os.access(webpath, os.W_OK):
+    shutil.copy(srcpath,webpath)
+  try:
+    os.chmod(srcpath,stat.S_IRWXU&(stat.S_IRUSR))
+  except:
+    pass
+  #  now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+  #  archive = webdir+'/archive/timeline-%s.json'%now
+  #  shutil.copy(srcpath,archive)
+  #  cmd = ['find',webdir+'/archive/','-name','*.json','-ctime','+14','-delete']
+  #  subprocess.check_output(cmd)
 
 def tail_log(job, nlines):
   print(''.ljust(80,'#'))
