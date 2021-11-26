e90d38f847a0d871a8be64eae632b0419d4b3a15
Nathan Baltzell <baltzell@gmx.com>
2021-11-26 07:55:03 -0500

    add run groups

--- disk-monitoring/report.sh
+++ disk-monitoring/report.sh
@@ -19,7 +19,7 @@ touch log
 echo "STARTING ..." >> log
 date >> log
 
-for xx in rg-a rg-b rg-k rg-f users
+for xx in rg-a rg-b rg-k rg-f rg-m users
 do
     indir=/work/clas12
     if [ $xx == "users" ]
@@ -39,7 +39,8 @@ du -s /work/clas12/* 2> perms.txt 1> du.txt
 du -s /work/clas12/users/* 2> permsA.txt 1> duA.txt
 cat duA.txt >> du.txt
 awk -F'[‘’]' '{print$2}' ./perms.txt > perms2.txt
-sort -r -n du.txt | awk '{printf"%12s %s\n",$1,$2}' > du2.txt
+df /work/clas12 > du2.txt
+sort -r -n du.txt | awk '{printf"%12s %s\n",$1,$2}' >> du2.txt
 mv -f du2.txt du.txt
 rm -f duA.txt permsA.txt
 echo 'These private directories occupy an unknown amount of disk space:' > perms.txt
@@ -48,7 +49,7 @@ echo >> perms.txt
 cat perms2.txt >> perms.txt
 rm -f perms2.txt
 
-scp -p *.txt *.html clas12@ifarm1901:~/clasweb/clas12offline/disk/work
+scp -p *.txt *.html clas12@ifarm1901:/u/group/clas/www/clasweb-2015/html/clas12offline/disk/work
 
 echo "FINISHED." >> log
 date >> log
