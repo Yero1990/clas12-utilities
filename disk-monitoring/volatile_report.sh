42d5d2d80401ac65e1fba8d3ae7790e0b242cea4
Nathan Baltzell <baltzell@gmx.com>
2021-11-29 08:48:48 -0500

    finally, fix writing data outside repo

--- disk-monitoring/volatile_report.sh
+++ disk-monitoring/volatile_report.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+SCRIPTDIR=`dirname $0`
+
 mkdir -p $HOME/disk
 cd $HOME/disk
 
@@ -7,16 +9,16 @@ export PYTHONPATH=/group/clas12/packages/mysql-connector/8.0.17/lib
 
 rm -f index.html cache.html hps-volatile.html hps-cache.html
 
-./volatile_html.py >& index.html
+$SCRIPTDIR/volatile_html.py >& index.html
 scp index.html clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/disk/volatile
 
-./cache_html.py >& cache.html
+$SCRIPTDIR/cache_html.py >& cache.html
 scp cache.html clas12@ifarm1901:/group/clas/www/clasweb/html/clas12offline/disk/cache/index.html
 
-./volatile_html.py /volatile/hallb/hps >& hps-volatile.html
+$SCRIPTDIR/volatile_html.py /volatile/hallb/hps >& hps-volatile.html
 scp hps-volatile.html hps@ifarm1901:/group/hps/www/hpsweb/html/disk/volatile/index.html
 
 # doing it from /cache/hallb/hps doesn't work, probably need to modify query
-./cache_html.py /cache/hallb >& hps-cache.html
+$SCRIPTDIR/cache_html.py /cache/hallb >& hps-cache.html
 scp hps-cache.html hps@ifarm1901:/group/hps/www/hpsweb/html/disk/cache/index.html
 
